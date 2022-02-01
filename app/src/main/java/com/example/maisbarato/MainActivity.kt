79998c8c7c2638466be6f012b328214ce8205dd9
Rnnfranca <renanfranca.contato@hotmail.com>
2022-02-01 14:28:08 -0300

    configuracao toolbar

--- app/src/main/java/com/example/maisbarato/MainActivity.kt
+++ app/src/main/java/com/example/maisbarato/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.maisbarato
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.maisbarato.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var navController: NavController
+    private lateinit var destinationListener: NavController.OnDestinationChangedListener
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
@@ -21,9 +27,32 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
-        setupActionBarWithNavController(navController)
+        binding.myToolbar.setupWithNavController(navController)
+//        setupActionBarWithNavController(navController)
+
+
+
+        destinationListener =
+            NavController.OnDestinationChangedListener { _, destination, arguments ->
+
+                binding.myToolbar.visibility = if (destination.id == R.id.loginFragment) {
+                    View.GONE
+                } else {
+                    View.VISIBLE
+                }
+
+                if (destination.id == R.id.listaOfertasFragment) {
+                    binding.myToolbar.navigationIcon = null
+                }
+
+            }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        navController.addOnDestinationChangedListener(destinationListener)
     }
 
     override fun onSupportNavigateUp(): Boolean {
